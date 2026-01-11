60a25cbc7e9793400d36093ba9d5fecc16b34698
Satish <satish.raut777@gmail.com>
2026-01-11 11:30:47 +0530

    Initail commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello DevOps 🚀 FastAPI inside Docker"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "OK"}
